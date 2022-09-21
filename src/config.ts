878f03af3f3d70a54c0ab35194b5763dfb5a7acf
Philip Lewis <philipjonlewis@gmail.com>
2022-09-21 09:01:18 +0800

    fixed env backend url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,4 @@
 export const config = {
-//   environment: import.meta.env.ENVIRONMENT,
-  backendURL:
-    import.meta.env.ENVIRONMENT == 'production'
-      ? 'https://restoplusmongodb.herokuapp.com/'
-      : 'http://192.168.0.25:4000/',
+  //   environment: import.meta.env.ENVIRONMENT,
+  backendURL: import.meta.env.BACKENDURL,
 };
